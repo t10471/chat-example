846c9f4e1b8b65c50e19541e3492b3faaafeb843
t10471 <t104711202@gmail.com>
2018-07-02 20:19:16 +0900

    wip

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate futures;
+extern crate bytes;
+extern crate http;
+extern crate httparse;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate time;
+extern crate tokio;
+extern crate tokio_codec;
+extern crate tokio_io;
+
+mod chat;
+mod rpc;
+
+use tokio::runtime::Runtime;
+use tokio::prelude::*;
+
+pub fn  main() {
+    println!("hello wold" );
+   let mut rt = Runtime::new().unwrap();
+    chat:: start_server(&mut rt);
+    println!("chat server running on localhost:6142");
+    rpc:: start_server(&mut rt);
+    println!("rpc server running on 127.0.0.1:8080");
+   rt.shutdown_on_idle().wait().unwrap();
+}
